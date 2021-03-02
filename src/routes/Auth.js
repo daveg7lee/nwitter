368b49a83853d6e3442ea1fd0d9c5e1761e3501c
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-03-02 19:04:49 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "../fbase";
 import useInput from "../hooks/useInput";
 
 const Auth = () => {
   const email = useInput("");
   const password = useInput("");
-  console.log(email.value, password.value);
+  const [newAccount, setNewAccount] = useState(false);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email.value,
+          password.value
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(
+          email.value,
+          password.value
+        );
+      }
+      console.log(data);
+    } catch (e) {
+      // to Do: handling Eror
+      console.log(e);
+    }
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
-          type="text"
+          type="email"
           placeholder="Email"
           value={email.value}
           onChange={email.onChange}
@@ -22,7 +44,7 @@ const Auth = () => {
           onChange={password.onChange}
           required
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? "Create Accont" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
