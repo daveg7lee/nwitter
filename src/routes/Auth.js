8c38827d9fef4d77026b3072a8435fb876986ae8
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-03-02 18:46:47 +0900

    Login Form part One

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,35 @@
 import React from "react";
+import useInput from "../hooks/useInput";
 
-const Auth = () => "Auth";
+const Auth = () => {
+  const email = useInput("");
+  const password = useInput("");
+  console.log(email.value, password.value);
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email.value}
+          onChange={email.onChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password.value}
+          onChange={password.onChange}
+          required
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
