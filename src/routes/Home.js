a872eba8529f2e7d0d08a68a87839e4271b7e139
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-03-03 19:58:11 +0900

    Getting the Nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
 import useInput from "../hooks/useInput";
 
 const Home = () => {
   const nweet = useInput("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const data = await dbService.collection("nweets").get();
+    data.forEach((document) => {
+      const nweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
   const onSubmit = async (e) => {
     e.preventDefault();
     await dbService
@@ -14,7 +28,7 @@ const Home = () => {
   return (
     <div>
       <form onSubmit={onSubmit} className="flex justify-center items-center">
-        <div className=" rounded-md shadow-sm w-80 mr-2">
+        <div className="rounded-md shadow-sm w-80 mr-2">
           <input
             type="text"
             placeholder="What's on your mind?"
@@ -30,6 +44,13 @@ const Home = () => {
           className="button border-blue-600 bg-blue-600 hover:bg-blue-500"
         />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
