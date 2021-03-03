a4a44f9355d036a1476f63d7703b7fe658919441
이기현 <dave@igihyeon-ui-MacBookAir.local>
2021-03-03 21:00:13 +0900

    Delete and Update part Two

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,22 +1,69 @@
-import React from "react";
-import { IoTrashOutline, IoPencilSharp } from "react-icons/io5";
+import React, { useState } from "react";
+import {
+  IoTrashOutline,
+  IoPencilSharp,
+  IoCloseOutline,
+  IoArrowForwardOutline,
+} from "react-icons/io5";
+import { toast } from "react-toastify";
+import { dbService } from "../fbase";
+import useInput from "../hooks/useInput";
 
 const Nweet = ({ nweet, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const newNweet = useInput(nweet.text);
+  const onDeleteClick = async () => {
+    try {
+      const ok = window.confirm("Are you sure you want to delete this nweet?");
+      if (ok) {
+        await dbService.doc(`nweets/${nweet.id}`).delete();
+      }
+    } catch (e) {
+      toast.error(e.message);
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    toggleEditing();
+    await dbService.doc(`nweets/${nweet.id}`).update({ text: newNweet.value });
+  };
   return (
     <div className="h-10 flex justify-center items-center bg-white shadow rounded-full py-6 px-4 my-1.5">
-      <h4 className="mr-2">{nweet.text}</h4>
-      <div className="flex items-center">
-        {isOwner && (
-          <>
-            <button className="mr-1 focus:outline-none">
-              <IoTrashOutline />
-            </button>
-            <button className="focus:outline-none">
-              <IoPencilSharp />
-            </button>
-          </>
-        )}
-      </div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              value={newNweet.value}
+              onChange={newNweet.onChange}
+              required
+            />
+          </form>
+          <button onClick={toggleEditing} className="focus:outline-none">
+            <IoCloseOutline className="text-xl" />
+          </button>
+        </>
+      ) : (
+        <>
+          <h4 className="mr-2">{nweet.text}</h4>
+          <div className="flex items-center">
+            {isOwner && (
+              <>
+                <button
+                  onClick={onDeleteClick}
+                  className="mr-1 focus:outline-none"
+                >
+                  <IoTrashOutline />
+                </button>
+                <button onClick={toggleEditing} className="focus:outline-none">
+                  <IoPencilSharp />
+                </button>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
